bd8142f2579757a90fb63e713f8f3caf1cd27c0d
Katirene <kat.traxler@gmail.com>
2016-02-28 21:30:53 -0600

    new dropdown menu with angular select working and changing paths

--- public/scripts/controllers/DropdownCtrl.js
+++ public/scripts/controllers/DropdownCtrl.js
@@ -1,30 +1,30 @@
-myApp.controller('DropdownCtrl', function ($scope, $log) {
-    $scope.items = [
-        {name:'Home', value: 'home', href: '#home'},
-        {name:'Dog', value: 'dog', href: '#findapet'},
-        {name: 'Cat', value: 'cat', href: '#findapet'},
-        {name: 'Bird', value: 'bird', href: '#findapet'},
-        {name: 'Small Fury', value: 'smallfury', href: '#findapet'}
-    ];
-
-    $scope.setChoice = function(data){
-        $scope.loc = data;
-        //Do somethign else..
-    };
-
-    $scope.status = {
-        isopen: false
-    };
-
-    $scope.toggled = function(open) {
-        $log.log('Dropdown is now: ', open);
-    };
-
-    $scope.toggleDropdown = function($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        $scope.status.isopen = !$scope.status.isopen;
-    };
-
-    $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
-});
+//myApp.controller('DropdownCtrl', function ($scope, $log) {
+//    $scope.items = [
+//        {name:'Home', value: 'home', href: '#home'},
+//        {name:'Dog', value: 'dog', href: '#findapet'},
+//        {name: 'Cat', value: 'cat', href: '#findapet'},
+//        {name: 'Bird', value: 'bird', href: '#findapet'},
+//        {name: 'Small Fury', value: 'smallfury', href: '#findapet'}
+//    ];
+//
+//    $scope.setChoice = function(data){
+//        $scope.loc = data;
+//        //Do somethign else..
+//    };
+//
+//    $scope.status = {
+//        isopen: false
+//    };
+//
+//    $scope.toggled = function(open) {
+//        $log.log('Dropdown is now: ', open);
+//    };
+//
+//    $scope.toggleDropdown = function($event) {
+//        $event.preventDefault();
+//        $event.stopPropagation();
+//        $scope.status.isopen = !$scope.status.isopen;
+//    };
+//
+//    $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
+//});
